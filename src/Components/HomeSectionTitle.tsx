87e061853d657da75a1c94ee37e0d09d4f810603
myusername <himashawije5678@gmail.com>
2024-11-07 15:02:38 +0530

    home section title effects completed

--- src/Components/HomeSectionTitle.tsx
+++ src/Components/HomeSectionTitle.tsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import { Typography } from '@mui/material';
-import "../Styles/HomeSectionTitle.css"
+import React from "react";
+import "../Styles/HomeSectionTitle.css";
 interface HomeSectionTitleProps {
-    homesectiontitle: any;
-  }
-  
-  export const HomeSectionTitle: React.FC<HomeSectionTitleProps> = ({
-    homesectiontitle,
-  }) => {
-  return (
+  homesectiontitle: any;
+}
 
+export const HomeSectionTitle: React.FC<HomeSectionTitleProps> = ({
+  homesectiontitle,
+}) => {
+  return (
     <p
-    className='home-section-title'
-    style={{ 
-        color: "#a3872d", 
+      className="home-section-title"
+      style={{
+        color: "#a3872d",
         marginTop: "20px",
         marginBottom: "15px",
-        
-      
-     }}
-    >{homesectiontitle}</p>
-  )
-}
+      }}
+    >
+      {homesectiontitle}
+    </p>
+  );
+};
